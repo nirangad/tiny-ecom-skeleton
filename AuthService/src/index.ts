e9a90aebcad86f4a0315c07d771f8cf815905e57
Niranga De Silva <nirangad@gmail.com>
2022-08-06 20:28:36 +0530

    Dockerized all services

--- AuthService/src/index.ts
+++ AuthService/src/index.ts
@@ -119,6 +119,7 @@ app.get(
 
 app.post("/auth/login", body("email").isEmail(), async (req, res) => {
   const result = validationResult(req);
+  console.log("Login validation: ", result);
   if (!result.isEmpty()) {
     return res
       .status(400)
@@ -134,6 +135,7 @@ app.post("/auth/login", body("email").isEmail(), async (req, res) => {
   }
 
   User.findOne({ email }, "+password", async (err: any, user: any) => {
+    console.log("User findOne error: ", err);
     if (err) {
       return res.status(500).json({ status: 0, message: req.t("HTTP_500") });
     }
